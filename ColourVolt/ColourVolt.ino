61d46a3fd41ba870eff3805cbf583540bd0e2364
James Fowkes <jamesfowkes@gmail.com>
2014-10-15 11:26:00 +0100

    Moved messaging to own module

--- ColourVolt/ColourVolt.ino
+++ ColourVolt/ColourVolt.ino
@@ -15,6 +15,7 @@
 
 /* Application Includes */
 #include "calibration.h"
+#include "messaging.h"
 
 /* Defines */
 #define RED_PIN 6
@@ -24,9 +25,6 @@
 #define VOLTS_PIN A0
 #define SETPOINT_PIN A1
 #define RANGE_PIN A2
-
-#define RX_PIN 9
-#define TX_PIN 10
 
 #define DEVICE_ID_EEPROM_ADDRESS 0x00 // 2 bytes
 #define CALIBRATION_EEPROM_ADDRESS 0x02 // 2 bytes
@@ -44,25 +42,24 @@ int rangeReading = 0;
 
 bool newReadingReady = false;
 
-SoftwareSerial softSerial(RX_PIN, TX_PIN);
-char deviceID[] = "??";
-
-char rxMessage[13];
-char rx = 0;
-char txMessage[13];
-
 void setup() {
 
   pinMode(RED_PIN, OUTPUT);    
   pinMode(GRN_PIN, OUTPUT); 
   pinMode(BLU_PIN, OUTPUT);
 
-  softSerial.begin(4800);
-  softSerial.println("ColourVolt");
+  delay(1000); // Wait a bit before sending anything over serial
+  
+  messagingBegin(4800, DEVICE_ID_EEPROM_ADDRESS);
 
-  deviceID[0] = EEPROM.read(DEVICE_ID_EEPROM_ADDRESS);
-  deviceID[1] = EEPROM.read(DEVICE_ID_EEPROM_ADDRESS + 1);
-
+  sendVoltage(0);
+  sendVoltage(1);
+  sendVoltage(10);
+  sendVoltage(100);
+  sendVoltage(1000);
+  sendVoltage(10000);
+  sendVoltage(30000);
+  
   // Give the calibration module its EEPROM address so it can read stored value
   Calib_setup(CALIBRATION_EEPROM_ADDRESS);
 
@@ -72,6 +69,11 @@ void setup() {
 
 void loop() 
 {
+  setLEDs(LOW, LOW, LOW);
+  delay(1000);
+  setLEDs(HIGH, LOW, LOW);
+  delay(1000);
+  /*
   if( newReadingReady )
   { 
     // There is a new ADC reading of the power input
@@ -83,7 +85,7 @@ void loop()
   readMainADCTask.tick();
   readRangeADCsTask.tick();
 
-  handleSerialComms();
+  handleSerialComms();*/
 }
 
 void processNewReading(void)
@@ -150,33 +152,9 @@ void readRangeADCs(void)
   }
 }
 
-void handleSerialComms(void)
+void handleRxMessage(char * message)
 {
-  if (softSerial.available())
-  {
-    char newChar = softSerial.read();
-
-    if (newChar != '\n')
-    {
-      rxMessage[rx++] = newChar;
-    }
-    else if (newChar == '\n')
-    {
-      handleRxMessage();
-      rx = 0;
-    }
-  }   
-}
-
-void handleRxMessage(void)
-{
-  if (isValidMessage() && deviceIDsMatch())
-  {
-    if (messageEquals("CHDEVID"))
-    {
-      setDeviceID(rxMessage[10], rxMessage[11]);
-    }
-    else if (messageEquals("V?"))
+    if (messageEquals("V?"))
     {
       unsigned int voltage = Calib_toMilliVolts(voltageReading);
       sendVoltage(voltage);
@@ -195,120 +173,6 @@ void handleRxMessage(void)
     {
       Calib_start();
     }
-  }
 }
 
-bool messageEquals(char * str)
-{
-  int len = strlen(str);
-  int i;
-
-  // Look through receive message chars starting at index 3 (after device ID)
-  for (i = 0; i < len; ++i)
-  {
-    // Return false as soon as a char doesn't match
-    if (rxMessage[3+i] != str[i]) { 
-      return false; 
-    }
-  }
-
-  // If here, must have matched all chars.
-  return true;
-}
-
-bool isValidMessage(void)
-{
-  // All messages should start with lowercase a
-  return rxMessage[0] == 'a';
-}
-
-bool deviceIDsMatch(void)
-{
-  // Both chars of device ID should match
-  bool match = true;
-  match &= (deviceID[0] == rxMessage[1]);
-  match &= (deviceID[1] == rxMessage[2]);
-
-  return match;
-}
-
-void setDeviceID(char a, char b)
-{
-      deviceID[0] = a;
-      deviceID[1] = b;
-      EEPROM.write(DEVICE_ID_EEPROM_ADDRESS, a);
-      EEPROM.write(DEVICE_ID_EEPROM_ADDRESS+1, b);
-}
-
-void sendVoltage(unsigned int mv)
-{
-  // Takes voltage in millivolts and sends to serial port (2 d.p.)
-  // e.g. 12345 mv gets sent as aXX12.34V where XX is the device ID
-  
-  initTxMessage();
-  
-  mv = mv / 10; // Only send two decimal places (so 12345 becomes 1234)
-  
-  // Convert numeric into message starting at 4th char
-  char length = 0;//toString(mv, &txMessage[3]) + 3;
-  
-  // Shift the last two chars to the right
-  // Then add decimal point , 'V' symbol and a new null terminator 
-
-  txMessage[length] = txMessage[length-1];
-  txMessage[length-1] = txMessage[length-2];
-  txMessage[length-2] = '.';
-  
-  txMessage[length + 1] = 'V';
-  txMessage[length + 2] = '\0';
-  
-  softSerial.println(txMessage);
-}
-
-void initTxMessage(void)
-{
-  txMessage[0] = 'a';
-  txMessage[1] = deviceID[0];
-  txMessage[2] = deviceID[2];
-}
-
-uint8_t toString(unsigned int val, char * buf)
-{
-	unsigned int  placevalue = 0;
-	uint8_t digitcount = 0;
-	bool foundSignificantDigit = false;
-	uint8_t bytesWritten = 0;
-	
-	for (placevalue = 1000; placevalue > 1; placevalue /= 10)
-	{
-		digitcount = getPlaceCount(val, placevalue);
-		if (digitcount || foundSignificantDigit)
-		{
-			foundSignificantDigit = true;
-			*buf++ = (char)(digitcount + '0');
-			val -= (digitcount * placevalue);
-			bytesWritten++;
-		}
-	}
-	
-	digitcount = val;
-	*buf++ = (char)(digitcount + '0');
-	bytesWritten++;
-	
-	return bytesWritten;
-}
-
-// getPlaceCount
-// - Get the magnitude at the specified digit in a number
-uint8_t getPlaceCount(unsigned int value, unsigned int place)
-{
-	if (place > 1 && value > 0)
-	{
-		return (uint8_t)(value / place);
-	}
-	else
-	{
-		return value - (10 * (value / 10));
-	}
-}
 
---
+++ ColourVolt/messaging.cpp
@@ -0,0 +1,272 @@
+/*
+
+ 	messsaging.cpp
+ 	
+ 	- Handles LLAP style messaging for ColourVolt
+ 	
+ */
+
+/* Arduino Library Includes */
+#include <Arduino.h>
+#include <EEPROM.h>
+#include <SoftwareSerial.h>
+
+/* Application Includes */
+#include "messaging.h"
+
+/* Defines and Typedefs */
+#define RX_PIN 9
+#define TX_PIN 10
+
+/* Extern Function Prototypes */
+extern void handleRxMessage(char * message); // Application message handler
+
+/* Private Function Prototypes */
+static uint8_t getPlaceCount(unsigned int value, unsigned int place);
+static uint8_t toString(unsigned int val, char * buf);
+
+/* Private Objects and Variables */
+static SoftwareSerial s_softSerial(RX_PIN, TX_PIN);
+static char s_deviceID[] = "??";
+
+static char s_rxMessage[13];
+static char s_rx = 0;
+static char s_txMessage[13];
+
+static int s_eepromAddress = 0;
+
+/* Private Function Definitions */
+
+static bool isValidMessage(void)
+{
+  // All messages should start with lowercase a
+  return s_rxMessage[0] == 'a';
+}
+
+static bool deviceIDsMatch(void)
+{
+  // Both chars of device ID should match
+  bool match = true;
+  match &= (s_deviceID[0] == s_rxMessage[1]);
+  match &= (s_deviceID[1] == s_rxMessage[2]);
+
+  return match;
+}
+
+static void initTxMessage(void)
+{
+  s_txMessage[0] = 'a';
+  s_txMessage[1] = s_deviceID[0];
+  s_txMessage[2] = s_deviceID[1];
+
+  char i;
+  for (i = 3; i < 13; i++)
+  {
+    s_txMessage[i] = '-';
+  }
+}
+
+static uint8_t toString(unsigned int val, char * buf)
+{
+  unsigned int  placevalue = 0;
+  uint8_t digitcount = 0;
+  bool foundSignificantDigit = false;
+  uint8_t bytesWritten = 0;
+
+  for (placevalue = 1000; placevalue > 1; placevalue /= 10)
+  {
+    digitcount = getPlaceCount(val, placevalue);
+    if (digitcount || foundSignificantDigit)
+    {
+      foundSignificantDigit = true;
+      *buf++ = (char)(digitcount + '0');
+      val -= (digitcount * placevalue);
+      bytesWritten++;
+    }
+  }
+
+  digitcount = val;
+  *buf++ = (char)(digitcount + '0');
+  bytesWritten++;
+
+  return bytesWritten;
+}
+
+// getPlaceCount
+// - Get the magnitude at the specified digit in a number
+static uint8_t getPlaceCount(unsigned int value, unsigned int place)
+{
+  if (place > 1 && value > 0)
+  {
+    return (uint8_t)(value / place);
+  }
+  else
+  {
+    return value - (10 * (value / 10));
+  }
+}
+
+static bool validIDChar(char id)
+{
+  bool valid = true;
+  valid &= (id <= 'Z');
+  valid &= (id >= 'A');
+  valid |= (id == '-');
+  valid |= (id == '#');
+  valid |= (id == '@');
+  valid |= (id == '?');
+  valid |= (id == '\\');
+  valid |= (id == '*');
+
+  return valid;
+}
+
+static bool setDeviceID(char a, char b)
+{
+  bool valid = validIDChar(a) && validIDChar(b);
+  if (valid)
+  {
+    s_deviceID[0] = a;
+    s_deviceID[1] = b;
+    EEPROM.write(s_eepromAddress, a);
+    EEPROM.write(s_eepromAddress+1, b);
+  }
+  return valid;
+}
+
+/* Public Function Defintions */
+
+void messagingBegin(int baud, int eepromAddress)
+{  
+  s_eepromAddress = eepromAddress;
+
+  s_deviceID[0] = EEPROM.read(s_eepromAddress);
+  s_deviceID[1] = EEPROM.read(s_eepromAddress + 1);
+
+  if (!validIDChar(s_deviceID[0]) || !validIDChar(s_deviceID[1]))
+  {
+    (void)setDeviceID('-', '-'); // Default to '--' as ID if EEPROM not valid
+  }
+
+  s_softSerial.begin(baud);
+
+  initTxMessage();
+  sendMessage("STARTED");
+}
+
+void handleSerialComms(void)
+{
+  if (s_softSerial.available())
+  {
+    // There is a new char ready in the serial buffer
+    char newChar = s_softSerial.read();
+
+    if ((newChar != '\n') && (newChar != '\r'))
+    {
+      // Not the end of a message, store char and exit
+      s_rxMessage[s_rx++] = newChar;
+    }
+    else
+    {
+      // End of message
+      if (isValidMessage() && deviceIDsMatch())
+      {
+        // Valid LLAP message, for this device
+        if (messageEquals("CHDEVID"))
+        {
+          // Instruction to change device ID - handle locally
+          if (setDeviceID(s_rxMessage[10], s_rxMessage[11]))
+          {
+            initTxMessage();
+            char * reply = "CHDEVID--";
+            reply[7] = s_deviceID[0];
+            reply[8] = s_deviceID[1];
+            sendMessage(reply);
+          }
+        }
+        else
+        {
+          // Pass message excluding 'aXX' to application
+          handleRxMessage(&s_rxMessage[3]);
+        }
+      }
+      s_rx = 0;
+    }
+  }   
+}
+
+bool messageEquals(char * str)
+{
+  int len = strlen(str);
+  int i;
+
+  // Look through receive message chars starting at index 3 (after device ID)
+  for (i = 0; i < len; ++i)
+  {
+    // Return false as soon as a char doesn't match
+    if (s_rxMessage[3+i] != str[i]) { 
+      return false; 
+    }
+  }
+
+  // If here, must have matched all chars.
+  return true;
+}
+
+void sendVoltage(unsigned int mv)
+{
+  // Takes voltage in millivolts and sends to serial port (2 d.p.)
+  // e.g. 12345 mv gets sent as aXX12.34V where XX is the device ID
+
+  initTxMessage();
+
+  mv = (mv + 5) / 10; // Only send two decimal places (with rounding, so 12345/10 becomes 1235)
+
+  // Convert numeric into message starting at 4th char
+  char * voltage = &s_txMessage[3];
+  char length = toString(mv, voltage);
+  
+  if (length == 1)
+  {
+    // Pad with leading zeros so that at 2 chars are written, e.g "1" becomes "01"
+    voltage[length] = voltage[length-1];
+    voltage[length-1] = '0'; 
+    length++;
+  }
+
+  if (length == 2)
+  {
+    // Pad with leading zero so that at 3 chars are written, e.g "01" becomes "001"
+    voltage[length] = voltage[length-1];
+    voltage[length-1] = voltage[length-2];
+    voltage[length-2] = '0'; 
+    length++;
+  }
+  
+  // Shift the last two chars to the right
+  // Then add decimal point and 'V' symbol
+  // e.g. "100" becomes "1.00"
+
+  voltage[length] = voltage[length-1];
+  voltage[length-1] = voltage[length-2];
+  voltage[length-2] = '.';
+  voltage[length + 1] = 'V';
+  s_softSerial.println(s_txMessage);
+}
+
+void sendMessage(char * message)
+{
+  char count = 0;
+
+  initTxMessage();
+
+  // Copy message into tx buffer
+  while (message[count] != '\0' && count < 9)
+  {
+    s_txMessage[count+3] = message[count];
+    count++;
+  }
+
+  s_softSerial.println(s_txMessage);
+}
+
